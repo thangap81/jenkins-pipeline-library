1773dfeb6ec81dbf2426d2919accc3fbb7f60a43
Prabhu <thangaraj.prabhu@gmail.com>
2021-04-28 23:22:25 +0800

    updated the test cmd

--- vars/sh.groovy
+++ vars/sh.groovy
@@ -1,9 +1,21 @@
 #!/usr/bin/env groovy
 
 def call(String cmd = "ls") {
+    
+    def result = new StringBuilder() //(1)
+    def error     = new StringBuilder()
 
-    println ("command parameter ${cmd}")
-    def cmdOutput = sh(returnStdout: true, script: cmd).trim()
-    println ("command output is ${cmdOutput}")
+    def comando = cmd.execute() //(2)
+    comando.consumeProcessOutput(resultado, error) //(3)
+    comando.waitForOrKill(1000) //(4)
+
+    if (!error.toString().equals("")) //(5)
+        println "Error"
+    else{
+        println "Executed successfully"
+        println result //(6)
+
+        }
+    
 }
 
