cb03d017d6c8c08f90005fd592275ecc20c901f1
Prabhu <thangaraj.prabhu@gmail.com>
2021-04-28 22:00:48 +0800

    updated the test cmd

--- vars/sh.groovy
+++ vars/sh.groovy
@@ -2,6 +2,7 @@
 
 def call(String cmd = "ls") {
 
+    println ("command parameter ${cmd}")
     def cmdOutput = sh(returnStdout: true, script: cmd).trim()
     println ("command output is ${cmdOutput}")
 }
